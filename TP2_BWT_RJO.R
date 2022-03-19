dcc338ae10bc2bacc06f56bc382eb51c06f7cd16
rosborn40383 <89399706+rosborn40383@users.noreply.github.com>
2022-03-19 11:35:42 -0400

    Add files via upload

--- TP2_BWT_RJO.R
+++ TP2_BWT_RJO.R
@@ -1,4 +1,4 @@
-install.packages("plotly")
+
 library(plotly)
 library(ggplot2)
 library(corrplot)
@@ -35,7 +35,7 @@ names(c)[25]<-"highway_mpg"
 names(c)[26]<-"price"
 describe(c,na.rm=TRUE,skew=FALSE)
 sapply(c,class)
-cc<-subset(c, select = c("horsepower","length","width","height","curb_weight","engine_size","bore","stroke","compression_ratio","city_mpg","highway_mpg","price")) 
+cc<-subset(c, select = c("normalized_losses","wheel_base","horsepower","length","width","height","curb_weight","engine_size","bore","stroke","compression_ratio","peak_rpm","city_mpg","highway_mpg","price")) 
 car<-na.omit(c)
 car
 cor(cc,cc)
@@ -51,5 +51,44 @@ sapply(cc,class)
 na.pass(cc)
 cor(cc, y = NULL, use = "na.or.complete")
 corr<- cor(cc, y = NULL, use = "na.or.complete")
-heatmap(corr)
+heatmap(corr, symm=TRUE,)
+as.data.frame(cc)
+categ<-subset(c, select = c("symboling","make","fuel_type","aspiration","num_of_doors","body_style","drive_wheels","engine_location","engine_type","num_of_cylinders","fuel_system"))
+#Scatter Plot
+ggplot(data=cc, mapping=aes(x=width, y=length, color=height)) +
+  geom_point() + ggtitle("Width vs. Length (by height)")
+
+#Continuous Variables
+ggplot(data=cc, mapping=aes(x=normalized_losses)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Normalized Losses')
+ggplot(data=cc, mapping=aes(x=wheel_base)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Wheel Base')
+ggplot(data=cc, mapping=aes(x=horsepower)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Horsepower')
+ggplot(data=cc, mapping=aes(x=length)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Length')
+ggplot(data=cc, mapping=aes(x=width)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Width')
+ggplot(data=cc, mapping=aes(x=height)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Height')
+ggplot(data=cc, mapping=aes(x=curb_weight)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Curb Weight')
+ggplot(data=cc, mapping=aes(x=engine_size)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Engine Size')
+ggplot(data=cc, mapping=aes(x=bore)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Bore')
+ggplot(data=cc, mapping=aes(x=stroke)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Stroke')
+ggplot(data=cc, mapping=aes(x=compression_ratio)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of compression Ration')
+ggplot(data=cc, mapping=aes(x=peak_rpm)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Peak RPM')
+ggplot(data=cc, mapping=aes(x=city_mpg)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of City MPG')
+ggplot(data=cc, mapping=aes(x=highway_mpg)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Highway MPG')
+ggplot(data=cc, mapping=aes(x=price)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Price')
+
+#Categorical Distributions
+ggplot(data=categ, mapping=aes(x=symboling)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Symboling')
+ggplot(data=categ, mapping=aes(x=make)) + geom_histogram(fill="blue", color='black') + ggtitle('Distribution of Make')
+ggplot(data=categ, mapping=aes(x=fuel_type)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Fuel Type')
+ggplot(data=categ, mapping=aes(x=aspiration)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Aspiration')
+ggplot(data=categ, mapping=aes(x=num_of_doors)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Number of Doors')
+ggplot(data=categ, mapping=aes(x=body_style)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Body Style')       
+ggplot(data=categ, mapping=aes(x=drive_wheels)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Drive Wheels')
+ggplot(data=categ, mapping=aes(x=engine_location)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Enigne Location')
+ggplot(data=categ, mapping=aes(x=engine_type)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Engine Type')
+ggplot(data=categ, mapping=aes(x=num_of_cylinders)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Number of Cylinders')
+ggplot(data=categ, mapping=aes(x=fuel_system)) + geom_histogram(fill="blue", color='black',stat="count") + ggtitle('Distribution of Fuel System')
+
+#PAIRPLOT
+
+pairs(cc)
 
